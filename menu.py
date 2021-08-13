952a2fc683768603ad72ece8ef69c15e1e69f98e
Derek King <king.dm49@gmail.com>
2021-08-13 20:57:17 +0800

    Redid the validate_input function to return None instead of False when invalid

--- menu.py
+++ menu.py
@@ -3,34 +3,34 @@ __author__ = 'Derek King'
 
 
 def validate_input(str_input, int_min=float("-inf"), int_max=float("inf")):
-    # Converts a string to an int within a given range or prints an error message
-    class RangeError(Exception):
-        pass
-
+    # Converts a string to an int within a given range
+    #  or prints an error message and returns None
     try:
         int_input = int(str_input)
-        if int_min <= int_input <= int_max:
-            return int_input
-        else:
-            raise RangeError
     except ValueError:
-        print("\n   Please enter a valid integer.")
-        return False
-    except RangeError:
+        # Int parsing fails
+        print("\n   Please enter a valid integer")
+        return None
+
+    if int_min <= int_input <= int_max:
+        # Valid int in range
+        return int_input
+    else:
         if int_max == float('inf'):
             print("\n   Please enter an integer greater than", int_min - 1)
         elif int_min == float('-inf'):
             print("\n   Please enter an integer less than", int_max + 1)
         else:
             print("\n   Please enter an integer between", int_min, "and", int_max)
-        return False
+        return None
 
 
 def menu(title, *arg):
     # A simple text menu with a variable number of options
+    #  Returns False if quit is selected and the selected option number otherwise
     n = len(arg)
-    selection = False
-    while not selection:
+    selection = None
+    while selection is None:
         print("\n" + title + ":")
         for i in range(n):
             print(str(i+1) + ". " + arg[i])
@@ -45,12 +45,12 @@ def menu(title, *arg):
 
 
 if __name__ == "__main__":
-    result = menu("Menu", "Option 1", "Option 2", "Option 3")
+    result = True
     while result:
+        result = menu("Menu", "Option 1", "Option 2", "Option 3")
         if result == 1:
             print("Option 1")
         elif result == 2:
             print("Option 2")
         elif result == 3:
             print("Option 3")
-        result = menu("Option 1", "Option 2", "Option 3")
